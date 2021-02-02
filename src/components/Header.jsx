091454a3a42970ddd60295bcb4993c51eb032266
strnpl <70884209+strnpl@users.noreply.github.com>
2021-02-02 17:29:27 +0300

    first push

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://i.pinimg.com/originals/3b/fa/47/3bfa47828d8d62b924f12ca314d6b400.jpg"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <a href="http://www.vk.com/strnpl">
+          <li>Profile</li>
+        </a>
+        <a href="#">
+          <li>Messages</li>
+        </a>
+        <a href="#">
+          <li>News</li>
+        </a>
+        <a href="#">
+          <li>Music</li>
+        </a>
+        <a href="#">
+          <li>Settings</li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
